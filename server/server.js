dcf28a14443e4f44b00e779c4e8e90eb4a85ff5a
Owais <mohdowaiskhan5155@gmail.com>
2026-06-11 02:41:00 +0530

    Update CORS

--- server/server.js
+++ server/server.js
@@ -14,7 +14,14 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173"
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
